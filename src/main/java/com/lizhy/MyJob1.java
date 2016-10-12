f4331156253757364ec7ea91965d0ece7fd1f5c4
lizhiyang <lizhiyang@yunzongnet.com>
2016-10-12 10:15:43 +0800

    quartz集群测试

---
+++ src/main/java/com/lizhy/MyJob1.java
@@ -0,0 +1,24 @@
+package com.lizhy;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by lizhiyang on 2016/10/10.
+ */
+@DisallowConcurrentExecution
+public class MyJob1 implements Job , Serializable{
+
+    private static final long serialVersionUID = -4816708186668603950L;
+
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd HH:MM:ss");
+        System.out.println("MyJob***1 execute==================="+format.format(new Date()));
+    }
+}
---
+++ src/main/java/com/lizhy/MyJob2.java
@@ -0,0 +1,26 @@
+package com.lizhy;
+
+import org.quartz.DisallowConcurrentExecution;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by lizhiyang on 2016/10/10.
+ */
+@DisallowConcurrentExecution
+public class MyJob2 implements Job , Serializable {
+
+    private static final long serialVersionUID = -4899329211299043729L;
+
+    public void execute(JobExecutionContext context) throws JobExecutionException {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-mm-dd HH:MM:ss");
+        System.out.println("MyJob***2 execute==================="+format.format(new Date()));
+    }
+}
